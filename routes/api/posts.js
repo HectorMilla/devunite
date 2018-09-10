e1784e708d01206ea517154d2c3474fc9bf5da87
hector milla <hectormilla8@gmail.com>
2018-09-10 19:15:30 -0400

    added route to register users from api

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
+// @route Get api/post/test
+// @desc tests post route
+// @access public
+
 router.get("/test", (req, res) => res.json({ msg: "posts works" }));
 
 module.exports = router;
