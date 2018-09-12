88a1aec6ad00941bdb94024816d039e302b56321
hector milla <hectormilla8@gmail.com>
2018-09-12 09:56:52 -0400

    added delete routes for edu and exp, created post model

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -236,4 +236,61 @@ router.post(
   }
 );
 
+// @route Delete api/profile/experience/:exp_id
+// @desc Delete experience from profile
+// @access Private
+router.delete(
+  "/experience/:exp_id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        // Get remove index
+        profile.experience = profile.experience.filter(
+          experience => experience.id != req.params.exp_id
+        );
+
+        // Save
+        profile.save().then(profile => res.json(profile));
+      })
+      .catch(err => res.status(404).json());
+  }
+);
+
+// @route Delete api/profile/education/:edu_id
+// @desc Delete education from profile
+// @access Private
+router.delete(
+  "/education/:edu_id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        // Get remove index
+
+        profile.education = profile.education.filter(
+          education => education.id != req.params.exp_id
+        );
+
+        // Save
+        profile.save().then(profile => res.json(profile));
+      })
+      .catch(err => res.status(404).json());
+  }
+);
+
+// @route Delete api/profile/
+// @desc Delete user and profile
+// @access Private
+router.delete(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOneAndRemove({ user: req.user.id }).then(() => {
+      User.findOneAndRemove({ _id: req.user.id }).then(() =>
+        res.json({ succes: true })
+      );
+    });
+  }
+);
 module.exports = router;
