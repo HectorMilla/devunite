17a9796c67516d1cd7b9973dcb841a3bf6fb8bc0
hector milla <hectormilla8@gmail.com>
2018-09-15 09:04:46 -0400

    added createProfile component and route

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,7 @@ import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import Footer from "./components/layout/Footer";
 import Dashboard from "./components/dashboard/Dashboard";
-
+import CreateProfile from "./components/create-profile/CreateProfile";
 import "./App.css";
 
 // Check for token
@@ -51,6 +51,13 @@ class App extends Component {
               <Route exact path="/login" component={Login} />
               <Switch>
                 <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              </Switch>
+              <Switch>
+                <PrivateRoute
+                  exact
+                  path="/create-profile"
+                  component={CreateProfile}
+                />
               </Switch>
             </div>
             <Footer />
